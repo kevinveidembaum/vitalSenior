cdc8df51046a265cd6bf2c2cce9e67e4091a162a
Kevin Nikolai <nikolai10052005@gmail.com>
2024-08-20 15:46:42 -0300

    feat: create data.py

---
+++ data.py
@@ -0,0 +1,7 @@
+   # Dicionário com perguntas e suas alternativas
+question_data = {
+        "Você ou sua família estão preocupados com novos problemas de memória?": ["Sim", "Não"],
+        "Você foi diagnosticado com demência, doença de Alzheimer ou um distúrbio neurocognitivo grave?": ["Sim", "Não"],
+        "No último ano, quantas vezes você foi internado em um hospital?": ["Nenhuma", "1 a 2 vezes", "Mais de 2 vezes"],
+        "Em geral, como você descreveria sua saúde?": ["Excelente", "Boa", "Regular", "Ruim"]
+    }
--- vital.py
+++ vital.py
@@ -1,14 +1,7 @@
 import flet as ft
+from data import question_data
 
 def main(page: ft.Page):
-    
-    # Dicionário com perguntas e suas alternativas
-    question_data = {
-        "Você ou sua família estão preocupados com novos problemas de memória?": ["Sim", "Não"],
-        "Você foi diagnosticado com demência, doença de Alzheimer ou um distúrbio neurocognitivo grave?": ["Sim", "Não"],
-        "No último ano, quantas vezes você foi internado em um hospital?": ["Nenhuma", "1 a 2 vezes", "Mais de 2 vezes"],
-        "Em geral, como você descreveria sua saúde?": ["Excelente", "Boa", "Regular", "Ruim"]
-    }
 
     # Armazenando respostas do usuário
     respostas = {}
